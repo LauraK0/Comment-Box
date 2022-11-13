f5a4bcc65d36168450004b1b1ab0dfacb7ca287f
Laura Keating <108976875+LauraK0@users.noreply.github.com>
2022-11-13 21:05:05 +0000

    Update website.js

--- website.js
+++ website.js
@@ -1,32 +1,56 @@
-// navigation bar
-const navToggle = document.querySelector(".navbar_toggle");
-const links = document.querySelector(".main_nav");
+const userComment = document.getElementById("message");
+const characterCount = document.getElementById("charNum");
 
-navToggle.addEventListener('click', function(){
-    links.classList.toggle("show_nav");
-})
 
-function myFunction(){
-    alert
-      let userInput = document.querySelector("#userInput");
-      let userOuput = document.querySelector("#userOuput");
-  
-      userOutput.innerHTML = "Thank you for submitting your comment, we have logged this as: " + userInput.value;
-  
+userComment.addEventListener("keyup", updateCharacterCounter);
+
+document.getElementById("contactForm").addEventListener("submit", validateMyForm);
+
+function updateCharacterCounter(){
+  const commentLength = userComment.value.length
+  characterCount.textContent = `${commentLength}/140 characters`;
+
+  if (commentLength > 140) {
+      userComment.classList.add("error");
+  } 
+
+  if (commentLength < 140 && userComment.classList.contains("error")) {
+    userComment.classList.remove("error");
+} 
+}
+
+function validateMyForm(e) {
+    let name = document.forms.contactForm.name.value;
+    let email = document.forms.contactForm.email.value;
+    let message = document.forms.contactForm.message.value;
+
+    let validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g; // Javascript reGex for email validation
+    let validateText = /\d+$/g; // Javascript reGex for Name validation
+
+    if (name == '' || name.trim().length === 0 || validateText.test(name)) {
+      e.preventDefault();
+      window.alert('Please tell us your name');
+      return false;
     }
-  
-  function countChars(obj){
-    document.getElementById("charNum").innerHTML = obj.value.length+' characters';
-  }
-  
-  function countChars(obj){
-    var maxLength = 140;
-    var strLength = obj.value.length;
-    
-    if(strLength > maxLength){
-        document.getElementById("charNum").innerHTML = '<span style="color: red;">'+strLength+' out of '+maxLength+' characters</span>';
-        document.getElementById("comments").value = '<span style="color: red;">';
-      }else{
-        document.getElementById("charNum").innerHTML = strLength+' out of '+maxLength+' characters';
+
+    if (email == '' || !validateEmail.test(email)) {
+      e.preventDefault();
+      window.alert('Please enter a valid e-mail address.');
+      return false;
     }
+
+    if (message == '' || message.trim().length === 0 || validateText.test(message)) {
+      e.preventDefault();
+      window.alert('Please enter your message.');
+      return false;
+    }
+
+    if (message.length >= 140) {
+      e.preventDefault();
+      window.alert('Your message is too long. Please shorten it before submitting.');
+      return false;
+    }
+
+    window.alert('Thank you for contacting us');
+    return true;
   }
